fd8790c5553ad0f80a4a963c4f971437cdd85f4b
Vincent Kjellman <vincent.kjellman@changetheagency.se>
2022-01-16 18:14:45 +0100

    Add imports for model script

---
+++ model.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import time
+from numpy import asarray
+from numpy import unique
+from numpy import argmax
+from tensorflow.keras.datasets.mnist import load_data
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPool2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
