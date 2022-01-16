6e5e8aaf4a9c6e220ea2470dabd6b5149b88d373
Vincent Kjellman <vincent.kjellman@changetheagency.se>
2022-01-16 19:33:21 +0100

    Fix indentions

--- predict.py
+++ predict.py
@@ -17,14 +17,14 @@ url = 'http://localhost:8501/v1/models/img_classifier:predict'
 
 # Call to endpoint
 def make_prediction(instances):
-   data = json.dumps({"signature_name": "serving_default", "instances": instances.tolist()})
-   headers = {"content-type": "application/json"}
-   json_response = requests.post(url, data=data, headers=headers)
-   predictions = json.loads(json_response.text)['predictions']
-   return predictions
+  data = json.dumps({"signature_name": "serving_default", "instances": instances.tolist()})
+  headers = {"content-type": "application/json"}
+  json_response = requests.post(url, data=data, headers=headers)
+  predictions = json.loads(json_response.text)['predictions']
+  return predictions
 
 predictions = make_prediction(x_test[0:4])
 
 # output predictions vs true data
 for i, pred in enumerate(predictions):
-   print(f"True Value: {y_test[i]}, Predicted Value: {np.argmax(pred)}")
+  print(f"True Value: {y_test[i]}, Predicted Value: {np.argmax(pred)}")
