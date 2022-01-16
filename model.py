e3a17f9fed712f909eeaad2fc9467788f77b966c
Vincent Kjellman <vincent.kjellman@changetheagency.se>
2022-01-16 18:17:25 +0100

    Normalize data from dataset

--- model.py
+++ model.py
@@ -10,3 +10,16 @@ from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPool2D
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dropout
+
+#load MNIST dataset
+(x_train, y_train), (x_test, y_test) = load_data()
+print(f'Train: X={x_train.shape}, y={y_train.shape}')
+print(f'Test: X={x_test.shape}, y={y_test.shape}')
+
+# reshape data to have a single channel
+x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], x_train.shape[2], 1))
+x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], x_test.shape[2], 1))
+
+# normalize pixel values
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
